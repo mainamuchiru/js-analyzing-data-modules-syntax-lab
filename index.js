ee5186bc5aace05fcf8c3709135dd2557651348e
mainamuchiru <mainaephantus@gmail.com>
2026-04-05 21:09:16 +0300

    index.js file updated

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-
-
-
+function combineUsers(...args) {
+    const combinedObject = {
+        users: []
+    };
+ 
+    for (const array of args) {
+        combinedObject.users = [...combinedObject.users, ...array];
+    }
+ 
+    combinedObject.merge_date = new Date().toString('M/d/yyyy');
+ 
+    return combinedObject;
+}
 module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
+  ...(typeof combineUsers !== "undefined" && { combineUsers }),
 };
